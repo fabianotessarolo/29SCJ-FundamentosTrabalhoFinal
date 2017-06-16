80dac85552ed9e8acd8e8b1f61b11fb474f8d6e9
Fabiano Tessarolo <fabiano.tessarolo@gmail.com>
2017-06-16 18:40:57 -0300

    Refactored removing Tweeter from main.

--- src/twitter/app/RunApp.java
+++ src/twitter/app/RunApp.java
@@ -3,6 +3,7 @@ package twitter.app;
 import twitter.configuration.Config;
 import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import twitter4j.conf.Configuration;
@@ -11,29 +12,20 @@ import twitter4j.conf.ConfigurationBuilder;
 public class RunApp {
 
 	public static void main(String[] args) {
-		
-		
-		Config config = new Config();		
-		try {
-			ConfigurationBuilder builder = new ConfigurationBuilder();
-			builder.setOAuthConsumerKey(config.getOAuthConsumerKey());
-			builder.setOAuthConsumerSecret(config.getOAuthConsumerSecret());
-			Configuration configuration = builder.build();
-			TwitterFactory factory = new TwitterFactory(configuration);
 
-			Twitter twitter = factory.getInstance();
-			AccessToken accessToken = loadAccessToken();
-			twitter.setOAuthAccessToken(accessToken);
-			Status status = twitter.updateStatus("Ola Twitter!");
+		Twitter twitter = new Factory().get();
+
+		String selectedHashTag = "#openjdk";
+
+			Status status = null;
+			
+			try {
+				status = twitter.updateStatus("Ola Twitter2!");
+			} catch (TwitterException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			System.out.println("Tweet postado com sucesso! [" + status.getText() + "].");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	private static AccessToken loadAccessToken() {
 		
-		Config config = new Config();
-		return new AccessToken(config.getAccessTokenKey(), config.getAccessTokenSecret());
 	}
 }
