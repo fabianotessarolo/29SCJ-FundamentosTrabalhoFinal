71c8669430966c3bb233142a290ff50cc789019f
Fabiano Tessarolo <fabiano.tessarolo@gmail.com>
2017-06-17 16:07:23 -0300

    Post working, counting working.

--- src/twitter/app/Operations.java
+++ src/twitter/app/Operations.java
@@ -2,7 +2,6 @@ package twitter.app;
 
 import java.text.SimpleDateFormat;
 import java.util.GregorianCalendar;
-import java.util.List;
 
 import twitter4j.Query;
 import twitter4j.QueryResult;
@@ -12,89 +11,83 @@ import twitter4j.TwitterException;
 
 public class Operations implements OperationsInterface {
 
+	/**
+	 * A utilização deste metodo não garante a quantidade correta de Tweets, e
+	 * possui limitação de Rate(gerando erros 429-Rate limit exceeded). Para o
+	 * trend correto, é necessária a utilização do Firehose que é pago, sendo
+	 * assim segue uma implementação acadêmica de exemplo utilizando o
+	 * Twitter4J.
+	 * 
+	 * Fonte:
+	 * https://stackoverflow.com/questions/26429965/twitter4j-count-the-number-of-tweets-within-24-hours-return-an-integer
+	 */
 	@Override
-	public int countTweetsLastWeek(String hashtag) {
-	
+	public int getTweetsLastWeek(String hashtag) {
+
 		Twitter twitter = Factory.get();
-		GregorianCalendar gCalendarInicio = new GregorianCalendar(2017, 5, 7);
-		System.out.println(gCalendarInicio.getTime());
-		GregorianCalendar gCalendarAgora = new GregorianCalendar();
-		//GregorianCalendar fCalendarAgora = new GregorianCalendar().getInstance();
-		//fCalendarAgora = gCalendarAgora.add(GregorianCalendar.DAY_OF_MONTH, -7);
-		System.out.println(gCalendarAgora.getTime());
-		SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
-		
+
+		GregorianCalendar gCalendarAgora = calendarNow();
+		GregorianCalendar gCalendarInicio = calendarPastWeek();
+
 		while (gCalendarInicio.before(gCalendarAgora)) {
 			Query query = new Query(hashtag);
-			GregorianCalendar gCalendarAnterior = new GregorianCalendar(); 
-			gCalendarAnterior.setTime(gCalendarInicio.getTime()); 
+			GregorianCalendar gCalendarAnterior = new GregorianCalendar();
+			gCalendarAnterior.setTime(gCalendarInicio.getTime());
 			gCalendarAnterior.add(GregorianCalendar.DAY_OF_MONTH, -1);
-			query.setSince(formatador.format(gCalendarAnterior.getTime()));
-			query.setUntil(formatador.format(gCalendarInicio.getTime()));
-			query.setCount(100);
+			query.setSince(formatador(gCalendarAnterior));
+			query.setUntil(formatador(gCalendarInicio));
+			query.setCount(1);
 			QueryResult result;
-			int contador=0;
+
+			int contadorDeTweets = 0;
+			int contadorDeRetweets = 0;
+			int contadorDeFavorites = 0;
+
 			try {
 				result = twitter.search(query);
 				while (result.hasNext()) {
 					query = result.nextQuery();
 					for (Status status : result.getTweets()) {
-				            contador++;
-				            
-				            /** if (contador>50){
-				            	System.out.println("Usuário: " + status.getUser().getScreenName());
-				            	System.out.println("Mensagem: " + status.getText());
-				            	System.out.println("Data de Criação: " + status.getCreatedAt());
-				            	System.out.println("Número de Favoritos: " + status.getFavoriteCount());
-				            	System.out.println("Geolocalização: " + status.getGeoLocation());
-				            	System.out.println("Lugar: " + status.getPlace());
-				            	System.out.println("Número de Retweets: " + status.getRetweetCount());
-				            }
-				            **/
+						contadorDeTweets++;
+						contadorDeRetweets = contadorDeRetweets + status.getRetweetCount();
+						contadorDeFavorites = contadorDeFavorites + status.getFavoriteCount();
 					}
-					result = twitter.search(query); 
+					result = twitter.search(query);
 				}
-				
-				System.out.println(formatador.format(gCalendarInicio.getTime())+": " + hashtag + contador+" tweets.");
+
+				System.out.println("\r--------------------------------------------\r" + formatador(gCalendarInicio)
+						+ ": " + hashtag + " " + contadorDeTweets + " tweets, " + contadorDeRetweets + " Retweets, "
+						+ contadorDeFavorites + " Favorites.");
 				gCalendarInicio.add(GregorianCalendar.DAY_OF_MONTH, 1);
 			}
-			
+
 			catch (Exception e) {
 				e.printStackTrace();
 			}
-			
-		}	
-		
-		return 0;
 
-	}
-
-	@Override
-	public int countRetweetsLastWeek(String hashtag) {
-		// TODO Auto-generated method stub
+		}
 		return 0;
 	}
 
-	@Override
-	public int countFavoritesLastWeek(String hashtag) {
-		// TODO Auto-generated method stub
-		return 0;
+	private GregorianCalendar calendarPastWeek() {
+		GregorianCalendar gCalendarInicio = new GregorianCalendar();
+		gCalendarInicio.add(GregorianCalendar.DAY_OF_MONTH, -7);
+		return gCalendarInicio;
 	}
 
-	@Override
-	public List<String> orderTweetsByAuthor(String hashtag) {
-		// TODO Auto-generated method stub
-		return null;
+	private GregorianCalendar calendarNow() {
+		GregorianCalendar gCalendarAgora = new GregorianCalendar();
+		return gCalendarAgora;
 	}
 
-	@Override
-	public List<String> orderTweetsByDate(String hashtag) {
-		// TODO Auto-generated method stub
-		return null;
+	private String formatador(GregorianCalendar date) {
+		SimpleDateFormat formatador = new SimpleDateFormat("yyyy-MM-dd");
+		return formatador.format(date.getTime());
 	}
 
 	@Override
 	public String tweet(String who, String message) {
+		System.out.println("Iniciando postagem de um Tweet: ");
 		Twitter twitter = Factory.get();
 		Status status = null;
 		try {
--- src/twitter/app/OperationsInterface.java
+++ src/twitter/app/OperationsInterface.java
@@ -1,13 +1,7 @@
 package twitter.app;
 
-import java.util.List;
-
 public interface OperationsInterface {
 	
-	public int countTweetsLastWeek(String hashtag);
-	public int countRetweetsLastWeek(String hashtag);
-	public int countFavoritesLastWeek(String hashtag);
-	public List<String> orderTweetsByAuthor(String hashtag);
-	public List<String> orderTweetsByDate(String hashtag);
+	public int getTweetsLastWeek(String hashtag);
 	public String tweet(String who, String message);
 }
